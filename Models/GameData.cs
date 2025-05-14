c9c724aed68439b468fe95b4fb0a6ebabea63964
Cyprian Zasada <120047588+cyptrix12@users.noreply.github.com>
2025-05-14 23:47:11 +0200

    Localhost true by default

--- Models/GameData.cs
+++ Models/GameData.cs
@@ -6,7 +6,7 @@ public class GameData
 {
     public int Revision { get; set; } = 1;
     public string LlmServerApi { get; set; } = "http://127.0.0.1:5000/";
-    public bool Localhost { get; set; } = false; 
+    public bool Localhost { get; set; } = true; 
     public bool FullScreen { get; set; } = false;
     public int GameWindowWidth { get; set; } = 1920;
     public int GameWindowHeight { get; set; } = 1080;
